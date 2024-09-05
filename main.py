cdf9f92c9561f2b0917d1e38cf8029593bc981b5
Ferhat <ferhatadibelli9@gmail.com>
2024-09-05 21:23:10 +0300

    pomodora app completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -6,15 +7,72 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_if_condition():
+    if reps == 0:
+        start_timer()
+    else:
+        print("Condition not met!")
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+def reset_timer():
+    global reps
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer", fg=GREEN)
+    checked_label.config(text="")
+    reps = 0
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+def start_timer():
+    global reps
+
+    work_sec = WORK_MIN * 60
+    short_sec = SHORT_BREAK_MIN * 60
+    long_sec = LONG_BREAK_MIN * 60
+
+    reps += 1
+
+    if reps % 8 == 0:
+        count_down(long_sec)
+        title_label.config(text="Break", fg=PINK)
+    elif reps % 2 == 0:
+        count_down(short_sec)
+        title_label.config(text="Break", fg=RED)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    global timer
+    minute = math.floor(count / 60)
+    seconds = count % 60
+
+    if 10 > seconds:
+        seconds = f"0{seconds}"
+
+    if count > 0:
+        canvas.itemconfig(timer_text, text=f"{minute}:{seconds}")
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        clicks = ""
+        for _ in range(math.floor(reps / 2)):
+            clicks += "✓"
+        checked_label.config(text=clicks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -24,19 +82,19 @@ window.config(padx=100, pady=80, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_ing = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_ing)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 title_label.grid(column=1, row=0)
 
-start_button = Button(text="Start", highlightthickness=0, borderwidth=0)
+start_button = Button(text="Start", highlightthickness=0, borderwidth=0, command=start_if_condition)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0, borderwidth=0)
+reset_button = Button(text="Reset", highlightthickness=0, borderwidth=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-checked_label = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30))
+checked_label = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30))
 checked_label.grid(column=1, row=2)
 
 window.mainloop()
